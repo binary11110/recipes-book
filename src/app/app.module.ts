230258c0343732bc781dab8f2aac821bf20af703
sara ahmed <sara.shehata00110011@gmail.com>
2022-09-04 11:10:30 +0200

    awl commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+// import { RecipeListComponent } from './recpies/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ShoppingListComponent,
+    RecipesComponent,
+    HeaderComponent,
+    ShoppingEditComponent,
+    // RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
